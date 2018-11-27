71b7ad61bc8398a063de44c66bcfe50bf0b65b15
Vikas Adiwal <adiwalv@gmail.com>
2018-11-27 19:43:35 +0530

    giving ui to basics

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>DBVisualizer</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="login-page">
+    <div class="login-box">
+        <div class="logo">
+            <a href="index.php">DB<b>Visualizer</b></a>
+            <small>Mongo DB explain() explained</small>
+        </div>
+        <div class="card">
+            <div class="body">
+                <form action = 'text.php' method="post">
+                            <div class="row clearfix">
+                                <div class="col-sm-12">
+                                         <select class="form-control show-tick" name = "db_name">
+                                        <option value="">-- Please select --</option>
+                                                                       <?php
+     require('./config/config.php');
+
+foreach($connection->listDatabases() as $database)
+{
+    echo "<option value = \"{$database->getName()}\">{$database->getName()}</option>";
+}
+?>
+                                    </select><br>
+				    <button class="btn btn-block btn-lg bg-blue waves-effect" type="submit">Submit</button>
+                                </div>
+				 
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery Core Js -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="plugins/node-waves/waves.js"></script>
+
+    <!-- Validation Plugin Js -->
+    <script src="plugins/jquery-validation/jquery.validate.js"></script>
+
+    <!-- Custom Js -->
+    <script src="js/admin.js"></script>
+    <script src="js/pages/examples/sign-in.js"></script>
+</body>
+
+</html>
